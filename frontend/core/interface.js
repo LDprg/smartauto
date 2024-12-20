be9025f1f16689acb1f6003a0e6a119e41056eee
LDprg <lukas_4dr@gmx.at>
2024-12-20 18:50:25 +0100

    fix frontend building

--- frontend/core/interface.js
+++ frontend/core/interface.js
@@ -1,12 +1,13 @@
 // JS wrapper for simplifing Rust access to extism
 
-import { createPlugin } from "./mod.js";
+import { createPlugin } from "/mod-ed1c65e98195f935.js";
 
 export async function runWasm() {
     const plugin = await createPlugin(
-        'https://github.com/extism/plugins/releases/latest/download/count_vowels.wasm'
+        "http://127.0.0.1:8080/test_plugin.wasm",
+        { useWasi: true }
     );
 
-    let out = await plugin.call("count_vowels", "Yellow, World!");
+    let out = await plugin.call("greet", "Yellow, World!");
     console.log(out.text());
 };
