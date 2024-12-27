22f2cc1799ad048adf90212104286437988678be
LDprg <lukas_4dr@gmx.at>
2024-12-28 00:07:48 +0100

    add utility to frontend

--- frontend/core/src/routes/+page.svelte
+++ frontend/core/src/routes/+page.svelte
@@ -2,17 +2,17 @@
 	import { createClient } from '@connectrpc/connect';
 	import { createGrpcWebTransport } from '@connectrpc/connect-web';
 
-	import { GreeterService, SayHelloRequestSchema } from '$lib/../gen/helloworld/v1/helloworld_pb';
+	import { GreeterService, SayHelloRequestSchema } from '$gen/helloworld/v1/helloworld_pb';
 	import { create } from '@bufbuild/protobuf';
 
 	async function callSayHello() {
 		const transport = createGrpcWebTransport({
-			baseUrl: 'http://127.0.0.1:3000'
+			baseUrl: 'http://127.0.0.1:3000',
 		});
 		const client = createClient(GreeterService, transport);
 
 		const message = create(SayHelloRequestSchema, {
-			name: 'web123'
+			name: 'web123',
 		});
 
 		console.log('Message: ' + message.name);
--- frontend/plugins/test_plugin/src/lib/Counter.svelte
+++ frontend/plugins/test_plugin/src/lib/Counter.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-  let count: number = $state(0)
-  const increment = () => {
-    count += 1
-  }
+	let count: number = $state(0);
+	const increment = () => {
+		count += 1;
+	};
 </script>
 
-<button onclick={increment} class="bg-blue-100 hover:bg-blue-400 rounded-full px-5 py-3">
-  count is {count}
+<button onclick={increment} class="rounded-full bg-blue-100 px-5 py-3 hover:bg-blue-400">
+	count is {count}
 </button>
