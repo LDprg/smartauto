b701e8ff5ddce160b4b3f5d30d2c3d98de010b35
LDprg <lukas_4dr@gmx.at>
2024-12-27 20:17:27 +0100

    add tailwindcss

--- frontend/core/src/my-element.ts
+++ frontend/core/src/my-element.ts
@@ -1,13 +1,13 @@
-import { LitElement, css, html } from 'lit';
+import { LitElement, html, unsafeCSS } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
-import litLogo from './assets/lit.svg';
-import viteLogo from '/vite.svg';
 
 import { createClient } from '@connectrpc/connect';
 import { createGrpcWebTransport } from '@connectrpc/connect-web';
 
 import { GreeterService, SayHelloRequestSchema } from './gen/helloworld/v1/helloworld_pb';
 import { create } from '@bufbuild/protobuf';
+
+import globalStyles from './index.css?inline';
 
 @customElement('my-element')
 export class MyElement extends LitElement {
@@ -23,7 +23,7 @@ export class MyElement extends LitElement {
   constructor() {
     super();
 
-    this.callSayHello();
+    // this.callSayHello();
   }
 
   private async callSayHello() {
@@ -48,19 +48,11 @@ export class MyElement extends LitElement {
   render() {
     return html`
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src=${viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://lit.dev" target="_blank">
-          <img src=${litLogo} class="logo lit" alt="Lit logo" />
-        </a>
-      </div>
-      <slot></slot>
-      <div class="card">
         <button @click=${this._onClick} part="button">count is ${this.count}</button>
       </div>
-      <p class="read-the-docs">${this.grpc}</p>
-      <p class="read-the-docs">${this.docsHint}</p>
+      <p class="bg-purple-950">${this.grpc}</p>
+      <p class="text-3xl font-bold underline">${this.docsHint}</p>
+      <div class="bg-purple-500 font-bold">Tailwind</div>
     `;
   }
 
@@ -68,77 +60,7 @@ export class MyElement extends LitElement {
     this.count++;
   }
 
-  static styles = css`
-    :host {
-      max-width: 1280px;
-      margin: 0 auto;
-      padding: 2rem;
-      text-align: center;
-    }
-
-    .logo {
-      height: 6em;
-      padding: 1.5em;
-      will-change: filter;
-      transition: filter 300ms;
-    }
-    .logo:hover {
-      filter: drop-shadow(0 0 2em #646cffaa);
-    }
-    .logo.lit:hover {
-      filter: drop-shadow(0 0 2em #325cffaa);
-    }
-
-    .card {
-      padding: 2em;
-    }
-
-    .read-the-docs {
-      color: #888;
-    }
-
-    ::slotted(h1) {
-      font-size: 3.2em;
-      line-height: 1.1;
-    }
-
-    a {
-      font-weight: 500;
-      color: #646cff;
-      text-decoration: inherit;
-    }
-    a:hover {
-      color: #535bf2;
-    }
-
-    button {
-      border-radius: 8px;
-      border: 1px solid transparent;
-      padding: 0.6em 1.2em;
-      font-size: 1em;
-      font-weight: 500;
-      font-family: inherit;
-      background-color: #1a1a1a;
-      cursor: pointer;
-      transition: border-color 0.25s;
-    }
-    button:hover {
-      border-color: #646cff;
-    }
-    button:focus,
-    button:focus-visible {
-      outline: 4px auto -webkit-focus-ring-color;
-    }
-
-    @media (prefers-color-scheme: light) {
-      a:hover {
-        color: #747bff;
-      }
-      button {
-        background-color: #f9f9f9;
-      }
-    }
-  `;
+  static styles = [unsafeCSS(globalStyles)];
 }
 
 declare global {
--- frontend/core/vite.config.ts
+++ frontend/core/vite.config.ts
@@ -1,21 +1,3 @@
 import { defineConfig } from 'vite';
 
-const importMap = {
-  //   lit: 'https://cdn.jsdelivr.net/gh/lit/dist@3/all/lit-all.min.js',
-};
-
-export default defineConfig({
-  plugins: [
-    {
-      name: 'importmap-resolver',
-      enforce: 'pre',
-      resolveId(source) {
-        // Check against your import map or conditions to rewrite the source
-        if (source in importMap) {
-          console.log(source);
-          return { id: importMap[source], external: true };
-        }
-      },
-    },
-  ],
-});
+export default defineConfig({});
